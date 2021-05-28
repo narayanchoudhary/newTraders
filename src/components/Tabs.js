289fb2d6db4cde390491274f75704dd1751505fe
unknown <narayanchoudhary>
2021-05-28 09:56:35 -0600

    falut

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -15,12 +15,12 @@ const useStyles = makeStyles({
     },
 });
 
-const groupBy = key => array =>
-    array.reduce((objectsByKeyValue, obj) => {
-        const value = obj[key];
-        objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
-        return objectsByKeyValue;
-    }, {});
+// const groupBy = key => array =>
+//     array.reduce((objectsByKeyValue, obj) => {
+//         const value = obj[key];
+//         objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+//         return objectsByKeyValue;
+//     }, {});
 
 export default function MyTabs() {
     const classes = useStyles();
